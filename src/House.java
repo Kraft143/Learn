f4cf1c665897fd5444682a07d2f2f485c8b5d156
kraft143 <kraft143@bk.ru>
2025-08-10 22:56:06 +0300

    first

---
+++ src/House.java
@@ -0,0 +1,9 @@
+public class House {
+    String name;
+    int square;
+
+    public House(String name, int square) {
+        this.name = name;
+        this.square = square;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String name = sc.nextLine();
+        System.out.println("Введите возраст: ");
+        int age = sc.nextInt();
+        System.out.println(name + " is " + age + " years old.");
+    }
+
+}
